a7a7fac2d1e79c859dd2398494ad2d02081b29d6
jpbaena13 <jpbaena13@hotmail.com>
2020-03-17 11:50:52 -0500

    Implementing UDCarousel and UDCollapse

--- C01/pages/01.jsx
+++ C01/pages/01.jsx
@@ -1,8 +1,30 @@
 import React from 'react'; // eslint-disable-line no-unused-vars
-import { Page, UDScrollbars } from 'unc-react-creator';
+import {
+  Page,
+  UDScrollbars,
+  UDCarousel,
+  UDCollapse
+} from 'unc-react-creator';
 import classNames from 'classnames';
 
 import Header from '../components/Header';
+
+const panels = [
+  {
+    title: '¿Sabes qué es impacto social? Escribe tu definición.',
+    content: (<textarea id='TA_1_before' cols='30' rows='10' placeholder='Ingresa tu respuesta' />)
+  },
+  {
+    title: `¿Sabes cómo puede influir el impacto social de un emprendimiento
+            en su sostenibilidad? Escribe en tu opinión.`,
+    content: (<textarea id='TA_2_before' cols='30' rows='10' placeholder='Ingresa tu respuesta' />)
+  },
+  {
+    title: `¿Conoces negocios del sector creativo y cultural que han capitalizado
+            el impacto social para su sostenibilidad económica? Escribe cuáles y de qué forma.`,
+    content: (<textarea id='TA_3_before' cols='30' rows='10' placeholder='Ingresa tu respuesta' />)
+  }
+];
 
 const CustomPage = (props) => (
   <Page {...props} className={classNames(props.className, 'unc-pecc-wrapper', 'unc-modulo00')}>
@@ -11,47 +33,31 @@ const CustomPage = (props) => (
       <UDScrollbars>
         <div className='unc-card'>
           <div className='unc-row'>
-            {/* eslint-disable */}
-            { /* TODO: Carousel */}
-            <div id='Carousel_1' className='carousel slide' data-ride='carousel'>
-              <ol className='carousel-indicators'>
-                <li data-target='#Carousel_1' data-slide-to='0' className='active'></li>
-                <li data-target='#Carousel_1' data-slide-to='1'></li>
-              </ol>
+            <UDCarousel steps={[
+              {
+                bgImage: './assets/img/M00_IMG06.jpg',
+                label: (
+                  <h1>
+                    El impacto social es el mayor aporte de las empresas creativas y culturales
+                  </h1>
+                )
+              },
+              {
+                bgImage: './assets/img/M00_IMG09.jpg',
+                label: (
+                  <div>
+                    <h3>
+                      Puedes capitalizar para la  sostenibilidad de tu emprendimiento
+                    </h3>
 
-              <div className='carousel-inner unc-equal-cols'>
-                <div className='item active col-'>
-                  <div className='unc-row-carousel unc-flex-center unc-row-bgfull' data-url='/content/pecc/imgs/M00_IMG06.jpg'>
-                    <div>
-                      <h1>EL IMPACTO SOCIAL ES EL MAYOR APORTE DE LAS EMPRESAS
-                      CREATIVAS Y CULTURALES</h1>
-                    </div>
+                    Tener claro, desde los objetivos de tu empresa creativa y cultural, qué
+                    impacto social generas en la comunidad, ya sea para resolver problemas
+                    comunes o mitigar otros, es clave a la hora de pensar en la sostenibilidad
+                    de tu negocio o emprendimiento.
                   </div>
-                </div>
-
-                <div className='item col-'>
-                  <div className='unc-row-carousel unc-flex-center unc-row-bgfull' data-url='/content/pecc/imgs/M00_IMG09.jpg'>
-                    <div style={{ textAlign: 'center' }}>
-                      <h3>Puedes capitalizar para la  sostenibilidad de tu emprendimiento</h3>
-                      Tener claro, desde los objetivos de tu empresa creativa y
-                      cultural, qué impacto social generas en la comunidad, ya sea para
-                      resolver problemas comunes o mitigar otros, es clave a la hora de
-                      pensar en la sostenibilidad de tu negocio o emprendimiento.
-                    </div>
-                  </div>
-                </div>
-              </div>
-
-              <a className='left carousel-control' href='#Carousel_1' data-slide='prev'>
-                <span className='ti-angle-left'></span>
-                <span className='sr-only'>Previous</span>
-              </a>
-              <a className='right carousel-control' href='#Carousel_1' data-slide='next'>
-                <span className='ti-angle-right'></span>
-                <span className='sr-only'>Next</span>
-              </a>
-            </div>
-          {/* eslint-enable */}
+                )
+              }
+            ]} />
           </div>
 
           <div className='unc-row unc-row-section-title'>
@@ -67,70 +73,30 @@ const CustomPage = (props) => (
             escribas en los campos de texto dando respuesta, desde tu experiencia, a las
             siguientes preguntas:
           </div>
-          {/* eslint-disable */}
-          { /* TODO: Collapse */}
-          <div className='unc-row'>
-            <div id='Collapse_1' className='panel-group' role='tablist' aria-multiselectable='true'>
-              <div className='panel panel-default'>
-                <div className='panel-heading' role='tab' id='Heading_1One'>
-                  <h5 className='panel-title'>
-                    <a role='button' data-toggle='collapse' data-parent='#Collapse_1' href='#Collapse_1One' aria-expanded='true' aria-controls='Collapse_1One'>
-                      ¿Sabes qué es impacto social? Escribe tu definición.
-                    </a>
-                  </h5>
-                </div>
-                <div id='Collapse_1One' className='panel-collapse collapse in' role='tabpanel' aria-labelledby='Heading_1One'>
-                  <div className='panel-body'>
-                    <textarea id='TA_1_before' cols='30' rows='10' placeholder='Ingresa tu respuesta'></textarea>
-                  </div>
-                </div>
-              </div>
-              <div className='panel panel-default'>
-                <div className='panel-heading' role='tab' id='Heading_1Two'>
-                  <h5 className='panel-title'>
-                    <a className='collapsed' role='button' data-toggle='collapse' data-parent='#Collapse_1' href='#Collapse_1Two' aria-expanded='false' aria-controls='Collapse_1Two'>
-                      ¿Sabes cómo puede influir el impacto social de un emprendimiento en su sostenibilidad? Escribe en tu opinión.
-                    </a>
-                  </h5>
-                </div>
-                <div id='Collapse_1Two' className='panel-collapse collapse' role='tabpanel' aria-labelledby='Heading_1Two'>
-                  <div className='panel-body'>
-                    <div className='unc-padding-1'>
-                      <textarea id='TA_2_before' cols='30' rows='10' placeholder='Ingresa tu respuesta'></textarea>
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className='panel panel-default'>
-                <div className='panel-heading' role='tab' id='Heading_1Three'>
-                  <h5 className='panel-title'>
-                    <a className='collapsed' role='button' data-toggle='collapse' data-parent='#Collapse_1' href='#Collapse_1Three' aria-expanded='false' aria-controls='Collapse_1Three'>
-                      ¿Conoces negocios del sector creativo y cultural que han capitalizado el impacto social para su sostenibilidad económica? Escribe cuáles y de qué forma.
-                    </a>
-                  </h5>
-                </div>
-                <div id='Collapse_1Three' className='panel-collapse collapse' role='tabpanel' aria-labelledby='Heading_1Three'>
-                  <div className='panel-body'>
-                    <div className='unc-padding-1'>
-                      <textarea id='TA_3_before' cols='30' rows='10' placeholder='Ingresa tu respuesta'></textarea>
-                    </div>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className='text-center'>
-              <button className='unc-button button-form' type='button' data-content-id='Collapse_1' data-module='unc-modulo00'>
-                <span className='btn-label'><i className='icon-speech'></i></span>
-                Descarga tus respuestas
-              </button>
-              <small style={{
-                display: 'block',
-                color:'#773939',
-                maxWidth: 600,
-                margin: '0 auto' }}>Ten presente que las respuestas no son almacenadas por el sistema. Por lo tanto deberás llenar las áreas de texto y a continuación hacer clic en <i>Descargar tus respuestas</i>.</small>
-            </div>
+
+          <UDCollapse
+            summaryClassName='unc-summary modulo00' panels={panels} />
+          <div className='text-center'>
+            <button
+              className='unc-button button-form'
+              type='button'
+              data-content-id='Collapse_1'
+              data-module='unc-modulo00'
+            >
+              <span className='btn-label'><i className='icon-speech'></i></span>
+              Descarga tus respuestas
+            </button>
+            <small style={{
+              display: 'block',
+              color: '#773939',
+              maxWidth: 600,
+              margin: '0 auto'
+            }}>
+              Ten presente que las respuestas no son almacenadas por el sistema.
+              Por lo tanto deberás llenar las áreas de texto y a continuación
+              hacer clic en <i>Descargar tus respuestas</i>.
+            </small>
           </div>
-          {/* eslint-enable */}
         </div>
       </UDScrollbars>
     </div>
